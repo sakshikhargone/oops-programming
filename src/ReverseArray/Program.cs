f3b5836aa8577a1be5c3489860c29e2a3947a7f3
sakshikhargone <sakshi.khargone@gmail.com>
2025-03-27 01:33:57 +0530

    Create array reversal program

---
+++ src/ReverseArray/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ReverseArray;
+string s = Console.ReadLine();
+string output = Solution.reverseArray(s);
+Console.WriteLine(output);
---
+++ src/ReverseArray/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    public class Solution
+    {
+        public static string reverseArray(string input)
+        {
+            StringBuilder s = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                s.Append(input[i]);
+            }
+            string result = s.ToString();
+            return result;
+        }
+    }
+}
